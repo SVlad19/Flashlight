343a854ecb7549db4d0abeb871af710b44ea49ae
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-03-27 20:36:43 +0300

    Finish

--- app/src/main/java/com/example/lab31foroop/MainActivity.kt
+++ app/src/main/java/com/example/lab31foroop/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab31foroop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.lab31foroop.databinding.ActivityMainBinding
 
 
@@ -13,11 +14,52 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val intent = Intent(this,MainActivity2::class.java)
         val view = binding.root
 
         setContentView(view)
+
+        binding.btnBlack.setOnClickListener(){
+            Log.d("MyLog","We are here")
+            intent.putExtra("Color","black")
+            startActivity(intent)
+        }
+        binding.btnBlue.setOnClickListener(){
+            intent.putExtra("Color","blue")
+            startActivity(intent)
+        }
+
+        binding.btnCian.setOnClickListener(){
+            intent.putExtra("Color","cyan")
+            startActivity(intent)
+        }
+
+        binding.btnGreen.setOnClickListener(){
+            intent.putExtra("Color","green")
+            startActivity(intent)
+        }
+
+        binding.btnLightGray.setOnClickListener(){
+            intent.putExtra("Color","lightGray")
+            startActivity(intent)
+        }
+
+        binding.btnMagenta.setOnClickListener(){
+            intent.putExtra("Color","magneta")
+            startActivity(intent)
+        }
+
+        binding.btnRed.setOnClickListener(){
+            intent.putExtra("Color","red")
+            startActivity(intent)
+        }
+
+        binding.btnWhite.setOnClickListener(){
+            intent.putExtra("Color","white")
+            startActivity(intent)
+        }
 
 
     }
--- app/src/main/java/com/example/lab31foroop/MainActivity2.kt
+++ app/src/main/java/com/example/lab31foroop/MainActivity2.kt
@@ -1,23 +1,38 @@
 package com.example.lab31foroop
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab31foroop.databinding.ActivityMain2Binding
 import com.example.lab31foroop.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
 
-    private lateinit var binding2: ActivityMainBinding
-
+    private lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-        binding2 = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding2.root
 
-        setContentView(view)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         val color = intent.getStringExtra("Color")
+
+        when(color){
+            "black"-> binding.CLMain2.setBackgroundColor(Color.BLACK)
+            "blue"->binding.CLMain2.setBackgroundColor(Color.BLUE)
+            "cyan"->binding.CLMain2.setBackgroundColor(Color.CYAN)
+            "green"->binding.CLMain2.setBackgroundColor(Color.GREEN)
+            "lightGray"->binding.CLMain2.setBackgroundColor(Color.LTGRAY)
+            "magneta"->binding.CLMain2.setBackgroundColor(Color.MAGENTA)
+            "red"->binding.CLMain2.setBackgroundColor(Color.RED)
+            "white"-> binding.CLMain2.setBackgroundColor(Color.WHITE)
+
+            else -> {
+                finish()
+            }
+        }
 
 
 
